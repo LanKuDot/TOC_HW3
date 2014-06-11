2cb5ad60ee6916f6f084417a2e76442b4a7ad5bf
LanKuDot <lankudot@github.com>
2014-06-12 00:57:45 +0800

    Get JSON data from webpage

---
+++ HW3/JsonWebReader.java
@@ -0,0 +1,32 @@
+/* Parsing the json file from the requested URL */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.*;
+
+public class JsonWebReader
+{
+	/* parsing the json file from the requested URL,
+	 * and return a JSONArray which stores multiple json entries. */
+	public static JSONArray readJsonFromURL( String url )
+		throws IOException, JSONException
+	{
+		InputStream input = new URL(url).openStream();
+		try
+		{
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader( input, Charset.forName("UTF-8") ) );
+			JSONArray json = new JSONArray( new JSONTokener( reader ) );
+			return json;
+		}
+		finally
+		{
+			input.close();
+		}
+	}
+}	// end of JsonWebReader class
